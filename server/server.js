99f661421fd5a01705da355a3320eb3fc9524d3e
Rahulab14 <abr14022007@gmail.com>
2025-04-30 11:04:23 +0530

    schema

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
 const express = require('express')
-const mongoose =require('mongoose')
+const mongoose = require('mongoose')
 const PORT = 3000 
 const app = express();
 app.use(express.json());
 
-app.listen(PORT,()=>{
+mongoose.connect('mongodb://localhost:27017/clickin')
+  .then(() => console.log('Connected to MongoDB'))
+ .catch(err => console.error('MongoDB connection error:', err));
+
++app.listen(PORT, () => {
     console.log(`server is running at ${PORT}`)
 })
