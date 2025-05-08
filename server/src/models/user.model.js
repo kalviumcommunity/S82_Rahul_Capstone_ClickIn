642c009172c8e1a343da710f56a852fec961d616
Rahulab14 <abr14022007@gmail.com>
2025-05-08 23:32:22 +0530

    Implemented authentication successfully

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,16 +1,4 @@
-// import mongoose from "mongoose";
 
-// const UserSchema = new mongoose.Schema(
-//   {
-//     email:      { type: String, required: true, unique: true },
-//     password:   { type: String, required: true },      
-//     name:       { type: String, required: true },
-//     phone:      String,
-//   },
-//   { timestamps: true }
-// );
-
-// export default mongoose.model("User", UserSchema);
 
 import mongoose from "mongoose";
 
